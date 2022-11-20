c72c57fdbe3551dfc3d9109d8241ff63e8f7718f
Isuranga Perera <isurangamperera@gmail.com>
2022-11-20 03:07:30 -0600

    Fixed missing logs

--- code/Configure.py
+++ code/Configure.py
@@ -4,12 +4,17 @@ import ImageUtils
 
 model_configs = {
     "save_dir": '../saved_models/',
+    "initial_lr": 0.1,
+    "max_epoch": 350,
+    "batch_size": 128,
+    "saved_models": '../saved_models/'
 }
 
 training_configs = {
     "initial_lr": 0.1,
     "max_epoch": 350,
     "batch_size": 128,
+    "saved_models": '../saved_models/'
 }
 
 ### END CODE HERE
