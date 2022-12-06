f821f84fc65ad194a1ff62d9d4369899f57d459b
Isuranga Perera <isurangamperera@gmail.com>
2022-12-06 02:07:28 -0600

    temp

--- code/Model.py
+++ code/Model.py
@@ -1,11 +1,13 @@
 import os
-
+import numpy as np
 import torch
 import torch.nn as nn
 from torch.optim.swa_utils import SWALR
 from tqdm import tqdm
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from Network import ResUnitBlock, ResNetV2Prop
+from torchvision import transforms
+from PIL import Image
 
 
 class MyModel(object):
@@ -77,13 +79,26 @@ class MyModel(object):
         return accuracy, correct, total
 
     def predict_prob(self, x):
+        norm_mean = (0.4914, 0.4822, 0.4465)
+        norm_std = (0.2470, 0.2435, 0.2616)
         self.network.eval()
         outputs = torch.empty(len(x), 10)
         with torch.no_grad():
             for idx in tqdm(range(x.shape[0])):
                 x_train = x[idx].reshape((3, 32, 32))
-                x_train = torch.tensor(x_train).type(torch.FloatTensor).unsqueeze(0).to('cuda')
-                output = self.network(x_train.float().cuda())
+                image = np.transpose(x_train, [1, 2, 0])
+
+                current_transform = transforms.Compose([  # defualt transform which only normalizes the data set
+                    transforms.ToTensor(),
+                    transforms.Normalize(norm_mean, norm_std)  # normalize dataset
+                ])
+
+                image = Image.fromarray(image)
+                image = current_transform(image)
+                image = np.transpose(image, [2, 0, 1])
+
+                image = torch.tensor(image).type(torch.FloatTensor).unsqueeze(0).to('cuda')
+                output = self.network(image.float().cuda())
                 outputs[idx * len(output):(idx + 1) * len(output)] = output
 
         return torch.nn.functional.softmax(outputs, dim=1).to('cpu').numpy()
